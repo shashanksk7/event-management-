904d1c268631969aea88831471358a0b74d20ada
Sanjayprajapati07 <sanjaycse0077@gmail.com>
2025-01-03 17:54:44 +0530

    code change

--- src/components/header.js
+++ src/components/header.js
@@ -1,2 +1,45 @@
-import React from 'react'
 
+
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { NavLink } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to={"/"}>
+            <h2>Glow More</h2>
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link "  to={"/products"}>
+                  Products
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link "  to={"/contact"}>
+                  Contact us
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
